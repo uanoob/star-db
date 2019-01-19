254711e69788681c0a160abab68dbe8531707f2f
uanoob <usnet@ukr.net>
2019-01-19 15:56:47 +0200

    Create base Header component & style

---
+++ src/components/header/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './header.css';
+
+const Header = () => (
+  <div className="header d-flex">
+    <h3>
+      <a href="/">StarDB</a>
+    </h3>
+    <ul className="d-flex">
+      <li>
+        <a href="/">People</a>
+      </li>
+      <li>
+        <a href="/">Planets</a>
+      </li>
+      <li>
+        <a href="/">Starships</a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Header;
