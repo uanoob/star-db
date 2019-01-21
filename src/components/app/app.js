0381b2620d497c45af423ec1c032864d4cb87872
uanoob <usnet@ukr.net>
2019-01-21 12:56:27 +0200

    Change App to statefull component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../header/header';
 import RandomPlanet from '../random-planet/random-planet';
 import ItemList from '../item-list/item-list';
 import PersonDetails from '../person-details/person-details';
 
-const App = () => (
-  <div>
-    <Header />
-    <RandomPlanet />
+class App extends Component {
+  state = {
+    selected: null,
+  };
 
-    <div className="row mb2">
-      <div className="col-md-6">
-        <ItemList />
+  onItemSelected = (id) => {
+    this.setState({
+      selected: id,
+    });
+  };
+
+  render() {
+    const { selected } = this.state;
+    return (
+      <div>
+        <Header />
+        <RandomPlanet />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onItemSelected} />
+          </div>
+          <div className="col-md-6">{selected ? <PersonDetails id={selected} /> : null}</div>
+        </div>
       </div>
-      <div className="col-md-6">
-        <PersonDetails />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default App;
