923642b0a19541036e0e59cd010be2b0d773848c
uanoob <usnet@ukr.net>
2019-01-21 12:52:27 +0200

    Connect PersonDetail to api

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,33 +1,70 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import './person-details.css';
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner/spinner';
 
-export default class PersonDetails extends Component {
-  state = {};
+class PersonDetails extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    person: null,
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.updatePerson();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { id } = this.props;
+    if (id !== prevProps.id) {
+      this.updatePerson();
+    }
+  }
+
+  updatePerson = () => {
+    const { id } = this.props;
+    if (!id) {
+      return;
+    }
+    this.swapiService
+      .getPerson(id)
+      .then((person) => {
+        this.setState({
+          person,
+          loading: false,
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
   render() {
-    return (
+    const { person, loading } = this.state;
+    return loading ? (
+      <Spinner />
+    ) : (
       <div className="person-details card">
         <img
           className="person-image"
-          src="https://starwars-visualguide.com/assets/img/characters/3.jpg"
-          alt="R2-D2"
+          src={`https://starwars-visualguide.com/assets/img/characters/${person.id}.jpg`}
+          alt={person.name}
         />
 
         <div className="card-body">
-          <h4>R2-D2</h4>
+          <h4>{person.name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Gender</span>
-              <span>male</span>
+              <span>{person.gender}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Birth Year</span>
-              <span>43</span>
+              <span>{person.birthYear}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Eye Color</span>
-              <span>red</span>
+              <span>{person.eyeColor}</span>
             </li>
           </ul>
         </div>
@@ -35,3 +72,9 @@ export default class PersonDetails extends Component {
     );
   }
 }
+
+PersonDetails.propTypes = {
+  id: PropTypes.string.isRequired,
+};
+
+export default PersonDetails;
