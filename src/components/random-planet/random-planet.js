8678f86df4cc80b08762595b076aed0df0d3f997
uanoob <usnet@ukr.net>
2019-01-21 12:54:08 +0200

    Add ErrorIndicator to RandomPlanet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -3,6 +3,7 @@ import SwapiService from '../../services/swapi-service';
 import './random-planet.css';
 import Spinner from '../spinner/spinner';
 import RandomPlanetView from './random-planet-view';
+import ErrorIndicator from '../error-indicator/error-indicator';
 
 export default class RandomPlanet extends Component {
   swapiService = new SwapiService();
@@ -10,32 +11,44 @@ export default class RandomPlanet extends Component {
   state = {
     planet: {},
     loading: true,
+    error: false,
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updatePlanet();
+    // this.interval = setInterval(this.updatePlanet, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   onPlanetLoaded = (planet) => {
     this.setState({ planet, loading: false });
   };
 
-  updatePlanet() {
-    const id = Math.floor(Math.random() * 15 + 2);
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
+  };
+
+  updatePlanet = () => {
+    const id = Math.floor(Math.random() * 25) + 2;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }
+      .catch(this.onError);
+  };
 
   render() {
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
     return (
       <div className="random-planet jumbotron rounded">
-        {loading ? <Spinner /> : <RandomPlanetView planet={planet} />}
+        {!(loading || error) ? <RandomPlanetView planet={planet} /> : null}
+        {loading ? <Spinner /> : null}
+        {error ? <ErrorIndicator /> : null}
       </div>
     );
   }
