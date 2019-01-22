dd54c3d4e38c6aa65e875d0c9ecb8b374554feda
uanoob <usnet@ukr.net>
2019-01-22 10:09:51 +0200

    Wrap PersonDetail to ErrorBoundry

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import './person-details.css';
 import SwapiService from '../../services/swapi-service';
 import Spinner from '../spinner/spinner';
+import ErrorBoundry from '../error-boundry/error-boundry';
+import ErrorButton from '../error-button/error-button';
 
 class PersonDetails extends Component {
   swapiService = new SwapiService();
@@ -41,34 +43,38 @@ class PersonDetails extends Component {
 
   render() {
     const { person, loading } = this.state;
-    return loading ? (
-      <Spinner />
-    ) : (
-      <div className="person-details card">
-        <img
-          className="person-image"
-          src={`https://starwars-visualguide.com/assets/img/characters/${person.id}.jpg`}
-          alt={person.name}
-        />
+    return (
+      <ErrorBoundry>
+        {loading ? <Spinner /> : null}
+        {person ? (
+          <div className="person-details card">
+            <img
+              className="person-image"
+              src={`https://starwars-visualguide.com/assets/img/characters/${person.id}.jpg`}
+              alt={person.name}
+            />
 
-        <div className="card-body">
-          <h4>{person.name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender</span>
-              <span>{person.gender}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Birth Year</span>
-              <span>{person.birthYear}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye Color</span>
-              <span>{person.eyeColor}</span>
-            </li>
-          </ul>
-        </div>
-      </div>
+            <div className="card-body">
+              <h4>{person.name}</h4>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <span className="term">Gender</span>
+                  <span>{person.gender}</span>
+                </li>
+                <li className="list-group-item">
+                  <span className="term">Birth Year</span>
+                  <span>{person.birthYear}</span>
+                </li>
+                <li className="list-group-item">
+                  <span className="term">Eye Color</span>
+                  <span>{person.eyeColor}</span>
+                </li>
+              </ul>
+              <ErrorButton />
+            </div>
+          </div>
+        ) : null}
+      </ErrorBoundry>
     );
   }
 }
