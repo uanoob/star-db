ae30d15d004f69ee1c0fd3ea9252b960ff341241
uanoob <usnet@ukr.net>
2019-01-22 12:37:36 +0200

    Create custom fields -> using Record component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,24 +2,28 @@ import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
 import Header from '../header/header';
 import RandomPlanet from '../random-planet/random-planet';
-import ItemList from '../item-list/item-list';
-import PersonDetails from '../person-details/person-details';
+// import ItemList from '../item-list/item-list';
+import ItemDetails from '../item-details/item-details';
+import Record from '../record/record';
 
 class App extends Component {
   swapiService = new SwapiService();
 
   state = {
-    selected: '3',
+    id: '5',
   };
 
   onItemSelected = (id) => {
     this.setState({
-      selected: id,
+      id,
     });
   };
 
   render() {
-    const { selected } = this.state;
+    const { id } = this.state;
+    const {
+      getPerson, getPersonImage, getStarship, getStarshipImage,
+    } = this.swapiService;
     return (
       <div>
         <Header />
@@ -27,14 +31,23 @@ class App extends Component {
 
         <div className="row mb2">
           <div className="col-md-6">
-            <ItemList
+            {/* <ItemList
               onItemSelected={this.onItemSelected}
               getData={this.swapiService.getAllPeople}
               renderItem={item => item.name}
-            />
+            /> */}
+            <ItemDetails id={id} getData={getPerson} getImageUrl={item => getPersonImage(item)}>
+              <Record label="Gender" field="gender" />
+              <Record label="Birth Year" field="birthYear" />
+              <Record label="Eye Color" field="eyeColor" />
+            </ItemDetails>
           </div>
           <div className="col-md-6">
-            <PersonDetails id={selected} />
+            <ItemDetails id={id} getData={getStarship} getImageUrl={item => getStarshipImage(item)}>
+              <Record label="Model" field="model" />
+              <Record label="Length" field="length" />
+              <Record label="Cargo Capacity" field="cargoCapacity" />
+            </ItemDetails>
           </div>
         </div>
       </div>
