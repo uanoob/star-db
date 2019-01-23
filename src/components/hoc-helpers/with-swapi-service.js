00212def7f76370869b46a627ffd6cdb73074465
uanoob <usnet@ukr.net>
2019-01-23 09:58:51 +0200

    Create hoc -> withSwapiService

---
+++ src/components/hoc-helpers/with-swapi-service.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SwapiServiceConsumer } from '../swapi-service-context/swapi-service-context';
+
+const withSwapiService = Wrapped => props => (
+  <SwapiServiceConsumer>
+    {swapiService => <Wrapped {...props} swapiService={swapiService} />}
+  </SwapiServiceConsumer>
+);
+
+export default withSwapiService;
